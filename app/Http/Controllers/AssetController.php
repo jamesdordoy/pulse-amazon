cb682f02620f237f0ba5f4f77fe9404410d042af
James Dordoy <jamesdordoy@gmail.com>
2020-11-06 16:51:32 +0000

    Added functionality to upload file to s3 and created simple listing

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -3,28 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Http\Requests\UploadRequest;
+
 
 class AssetController extends Controller
 {
     //
+    private $s3Client;
+
+    public function __construct()
+    {
+        $this->s3Client = \App::make('aws')->createClient('s3');
+    }
 
     public function showUpload()
     {
         return view('upload');
     }
 
-    public function upload()
+    public function upload(UploadRequest $request)
     {
+        $file = $request->file;
 
+        try {
+            $response = $this->s3Client->putObject(array(
+                'Bucket'     => 'jd-pulse-test-bucket',
+                'Key'        => $file->getClientOriginalName(),
+                'SourceFile' => $file->path(),
+            ));
+        }
+        catch (Exception $e) {
+            return abort(400);
+        }
+
+        return $response;
     }
 
     public function showDownload()
     {
-        return view('download');
-    }
+        $response = $this->s3Client->listObjects([
+            'Bucket' => 'jd-pulse-test-bucket',
+        ]);
 
-    public function download()
-    {
+        $images = $response->toArray()["Contents"];
 
+        return view('download', [
+            'images' => $images
+        ]);
     }
 }
--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -2,21 +2,22 @@
 
 @section('main')
 
-
-<table>
-    <thead>
-        <th>Filename</th>
-        <th></th>
-    </thead>
-    <tbody>
-        @foreach ($images as $image)
-            <tr>
-                <td>{{ $image['Key'] }}</td>
-                <td><a href="">Download</a></td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
-
+<h2>Download Images from S3</h2>
+<div class="table-responsive">
+    <table class="table table-striped table-bordered">
+        <thead>
+            <th>Filename</th>
+            <th></th>
+        </thead>
+        <tbody>
+            @foreach ($images as $image)
+                <tr>
+                    <td>{{ $image['Key'] }}</td>
+                    <td><a href="https://jd-pulse-test-bucket.s3.eu-west-1.amazonaws.com/{{ $image['Key'] }}">Download</a></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 
 @endsection
